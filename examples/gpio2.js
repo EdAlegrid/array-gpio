90ca204a4112b92b8d012db8050270257ea9829f
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-07-06 16:51:10 -0700

    Update gpio2.js

--- examples/gpio2.js
+++ examples/gpio2.js
@@ -6,28 +6,28 @@ const r = require('array-gpio');
 let sw = r.in({pin:[11,13], index:'pin'});
 let led = r.out({pin:[33,35,37,36,38,40], index:'pin'});
  
-let LedOn = ()=> {
-  let t = 0; // initial ON time delay in ms
-  for (let x in led){
-    t += 50;
-    led[x].on(t); 
-  }
+let LedOn = () => {
+        let t = 0; // initial ON time delay in ms
+        for (let x in led){
+                t += 50;
+                led[x].on(t); 
+        }
 }
  
-let LedOff = ()=> {
-  let t = 0; // initial OFF time delay in ms
-  for (let x in led){
-    t += 50;
-    led[x].off(t);
-  }
+let LedOff = () => {
+        let t = 0; // initial OFF time delay in ms
+        for (let x in led){
+                t += 50;
+                led[x].off(t);
+        }
 }
  
 r.watchInput(()=> {
-  if(sw[11].isOn){
-    LedOn();  // led's will turn ON sequentially with 50 ms delay
-  }
-  else if(sw[13].isOn){
-    LedOff(); // led's will turn OFF sequentially with 50 ms delay
-  }
+        if(sw[11].isOn){
+                LedOn();  // led's will turn ON sequentially with 50 ms delay
+        }
+        else if(sw[13].isOn){
+                LedOff(); // led's will turn OFF sequentially with 50 ms delay
+        }
 });
 
