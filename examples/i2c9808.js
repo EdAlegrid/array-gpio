6b62958c2e5d97f198e4e0ef71fa76ba4665dbd3
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-07-06 09:17:54 -0400

    Update i2c9808.js

--- examples/i2c9808.js
+++ examples/i2c9808.js
@@ -1,10 +1,9 @@
 /* Using MCP9808 ambient temperatute sensor */
 
-'use strict';
-
 const r = require('array-gpio');
 
-var i2c = r.setI2C();
+let i2c = r.startI2C(1); // using SDA1 and SCL1 pins (pin 3 & 5)
+//let i2c = r.startI2C(0); // using SDA0 and SCL0 pins (pin 27 & 28)
 
 /* set data transfer speed to 100 kHz */
 i2c.setTransferSpeed(200000);
@@ -21,58 +20,54 @@ const rbuf = Buffer.alloc(16); // read buffer
 
 /* Based on MCP9808 datasheet, compute the temperature */
 function computeTemp(){
+	let Temp;
+  	let UpperByte = rbuf[0], LowerByte = rbuf[1];
 
-  let Temp;
+ 	UpperByte = UpperByte & 0x1F; // Clear flag bits
 
-  let UpperByte = rbuf[0];
-  let LowerByte = rbuf[1];
-
-  UpperByte = UpperByte & 0x1F; // Clear flag bits
-
-  // Temp < 0°C
-  if ((UpperByte & 0x10) == 0x10){
-	UpperByte = UpperByte & 0x0F; // Clear SIGN
-
-	Temp = 256 - ((UpperByte * 16) + (LowerByte / 16));
-
-  // Temp > 0°C
-  }else { 
-	Temp = ((UpperByte * 16) + (LowerByte / 16));
-  }
-  return Temp;
+  	// Temp < 0°C
+  	if ((UpperByte & 0x10) == 0x10){
+		UpperByte = UpperByte & 0x0F; // Clear SIGN
+		Temp = 256 - ((UpperByte * 16) + (LowerByte / 16));
+  	// Temp > 0°C
+  	}
+  	else { 
+		Temp = ((UpperByte * 16) + (LowerByte / 16));
+  	}
+  	return Temp;
 }
 
 function precisionRound(number, precision) {
-  var factor = Math.pow(10, precision);
-  return Math.round(number * factor) / factor;
+  	var factor = Math.pow(10, precision);
+  	return Math.round(number * factor) / factor;
 }
 
 /* get temperature reading */
 var getTemp = exports.getTemp = function () {
 
-  /* access the internal 16-bit configuration register within MCP9808 */
-  wbuf[0] = 0x01; // address of configuration register
-  wbuf[1] = 0x02; // register upper byte, THYST set with +1.5 C
-  wbuf[2] = 0x00; // register lower byte (power up defaults)
-  i2c.write(wbuf, 3);
+  	/* access the internal 16-bit configuration register within MCP9808 */
+  	wbuf[0] = 0x01; // address of configuration register
+  	wbuf[1] = 0x02; // register upper byte, THYST set with +1.5 C
+  	wbuf[2] = 0x00; // register lower byte (power up defaults)
+  	i2c.write(wbuf, 3);
 
-  /* access the internal 16-bit ambient temp register within MCP9808 */
-  wbuf[0] = 0x05; // address of ambient temperature register
-  i2c.write(wbuf, 1);
+  	/* access the internal 16-bit ambient temp register within MCP9808 */
+  	wbuf[0] = 0x05; // address of ambient temperature register
+  	i2c.write(wbuf, 1);
 
-  /* read content of ambient temp register */
-  i2c.read(rbuf, 2); 
+  	/* read content of ambient temp register */
+  	i2c.read(rbuf, 2); 
 
-  /* function call to compute temperature */
-  var T = computeTemp();
-  //var t = precisionRound(T, 2);
-  var t = T.toFixed(2);
+  	/* function call to compute temperature */
+  	var T = computeTemp();
+  	//var t = precisionRound(T, 2);
+  	var t = T.toFixed(2);
   
-  return t;
+  	return t;
 }
 
 console.log('temp using i2c', getTemp());
 
-//i2c.end();
+i2c.end();
 //console.log('i2c closed');
 
