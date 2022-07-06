3bbc109ff7c51f25ed3bfde25b76b128ac4c83d1
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-07-06 16:48:51 -0700

    Update gpio1.js

--- examples/gpio1.js
+++ examples/gpio1.js
@@ -6,12 +6,13 @@ let sw = r.in(11);
 let led = r.out(33);
  
 /* Pressing the sw button will turn on the led,
-   releasing the sw button will turn off the led. */
+ * releasing the sw button will turn off the led.
+ */
 sw.watch((state) => {
-  if(state){
-  	led.on();
-  }
+  	if(state){
+  		led.on();
+  	}
 	else{
 		led.off();
-  }
+  	}
 }); 
